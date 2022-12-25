b2685f386045adaa64a14fd23993099c7f959fed
john katua <johnkatua99@gmail.com>
2022-12-25 17:48:41 +0300

    added route to send connection request

--- resources/routes/account.js
+++ resources/routes/account.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
+const { verifyToken } = require('../../middleware/verifyToken');
 const router = Router();
 
 const accountControllers = require('../controllers/account');
 
 router.post('/register', accountControllers.signUp);
 router.post('/login', accountControllers.login);
+router.post('/sendRequest', verifyToken, accountControllers.sendAConnectionRequest);
 
 module.exports = router;
