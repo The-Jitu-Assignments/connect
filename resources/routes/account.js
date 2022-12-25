0e3d9b4b838b9ff3011ae086295be9967a16e1b0
john katua <johnkatua99@gmail.com>
2022-12-25 13:10:09 +0300

    added a route to login

--- resources/routes/account.js
+++ resources/routes/account.js
@@ -4,5 +4,6 @@ const router = Router();
 const accountControllers = require('../controllers/account');
 
 router.post('/register', accountControllers.signUp);
+router.post('/login', accountControllers.login);
 
 module.exports = router;
