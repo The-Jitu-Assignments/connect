687beaa73e10523c0de299c486a1ae7d69d0a315
john katua <johnkatua99@gmail.com>
2022-12-25 13:05:59 +0300

    added a controller to login user

--- resources/controllers/account.js
+++ resources/controllers/account.js
@@ -1,6 +1,7 @@
 const sql = require('mssql');
 const bcrypt = require('bcrypt');
 const { v4 } = require('uuid');
+const jwt = require('jsonwebtoken');
 const sqlConfig = require('../../config');
 
 exports.signUp = async (req, res) => {
@@ -34,8 +35,39 @@ exports.signUp = async (req, res) => {
 
 exports.login = async (req, res) => {
   try {
-    
+    const pool = await sql.connect(sqlConfig);
+    const { email, password } = req.body;;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        msg: 'input all fields'
+      })
+    };
+
+    const user = await (await pool.request().input('email', email).execute('usp_findUser')).recordset[0];
+
+    if (user) {
+      const checkPwd = await bcrypt.compare(password, user.password);
+      if (checkPwd) {
+        const { password, ...payload } = user;
+        const token = jwt.sign(payload, process.env.SECRET, { expiresIn: '24h' })
+        return res.status(200).json({
+          msg: 'Logged in successfully',
+          token
+        })
+      } else {
+        return res.status(400).json({
+          msg: 'Invalid credentials'
+        })
+      }
+    } else {
+      return res.status(404).json({
+        msg: 'user with that email does not exist'
+      })
+    }
   } catch (error) {
-    
+    return res.status(500).json({
+      msg: error
+    })
   }
 };
