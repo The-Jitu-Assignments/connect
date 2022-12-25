ff56b6dc40a15c1c2c04d3d22e50ee0252273bbd
john katua <johnkatua99@gmail.com>
2022-12-25 18:32:43 +0300

    added a route to accept or decline a connection request

--- resources/routes/account.js
+++ resources/routes/account.js
@@ -7,5 +7,6 @@ const accountControllers = require('../controllers/account');
 router.post('/register', accountControllers.signUp);
 router.post('/login', accountControllers.login);
 router.post('/sendRequest', verifyToken, accountControllers.sendAConnectionRequest);
+router.put('/acceptOrDecline/:connectionId', verifyToken, accountControllers.acceptOrDeclineConnectionRequest);
 
 module.exports = router;
