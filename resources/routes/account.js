6e5d495efd4edef7cf2313ac00d16f43740e81a3
john katua <johnkatua99@gmail.com>
2022-12-25 10:51:13 +0300

    added account routes

---
+++ resources/routes/account.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const router = Router;
+
+const accountControllers = require('../controllers/account');
+
+router.post('/', accountControllers.signUp);
+
+module.exports = router;
