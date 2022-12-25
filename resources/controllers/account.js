9f55ca73ea5dd04ebb4a03c0ba1fdf43cd9aadbf
john katua <johnkatua99@gmail.com>
2022-12-25 10:42:24 +0300

    added a controller to create new account

--- resources/controllers/account.js
+++ resources/controllers/account.js
@@ -1,10 +1,35 @@
 const sql = require('mssql');
+const bcrypt = require('bcrypt');
+const { v4 } = require('uuid');
+const sqlConfig = require('../../config');
 
 exports.signUp = async (req, res) => {
   try {
-    
+    const pool = await sql.connect(sqlConfig);
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.json({
+        msg: 'Input all fields'
+      })
+    };
+
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    await pool.request()
+      .input('accountID', v4())
+      .input('name', name)
+      .input('email', email)
+      input('password', hashedPassword)
+    .execute('usp_accountRegister')
+
+    return res.status(200).json({
+      msg: 'account created successfully'
+    })
   } catch (e) {
-    
+    return res.status(500).json({
+      msg: error
+    })
   }
 };
 
