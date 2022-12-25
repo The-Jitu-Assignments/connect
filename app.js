e88479d76e5c85fbb6dcd2b37279ed842af9ce4d
john katua <johnkatua99@gmail.com>
2022-12-25 09:26:17 +0300

    updated the app js file

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('send connection request');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome');
+});
+
+app.listen(3000, () => {
+  console.log('Server running');
+})
