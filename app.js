f50d149f47c0a1f4f3fc27cee8879327222893e3
john katua <johnkatua99@gmail.com>
2022-12-25 09:35:42 +0300

    updated the git ignore file

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { PORT } = process.env || 5001;
 
 app.get('/', (req, res) => {
   res.send('Welcome');
 });
 
-app.listen(3000, () => {
-  console.log('Server running');
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
 })
