0cdc494baac9ef105772be59f81e944f998cda16
john katua <johnkatua99@gmail.com>
2022-12-26 19:05:10 +0300

    user can navigate to login page and back to home page

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <div className='header-container'>
       <h1>
         EquiKonnect
       </h1>
       <div className='header-auth'>
-        <div className='header-login'>Login</div>
+        <div className='header-login' onClick={() => navigate('/login')}>Login</div>
         <div className='header-register'>Register</div>
       </div>
     </div>
