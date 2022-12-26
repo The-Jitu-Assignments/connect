8110ddfbfc159ed32379debeb4795e448eaf5c02
john katua <johnkatua99@gmail.com>
2022-12-26 15:33:00 +0300

    deleted the home css file

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Header from '../../components/header/Header'
-import './Home.css'
 
 const Home = () => {
   return (
-    <div className='home-container'>
+    <>
       <Header />
       <div>Main</div>
       <div>Footer</div>
-    </div>
+    </>
   )
 }
 
