3b58ac2f094d50cded8c783755479d58406b09fe
john katua <johnkatua99@gmail.com>
2022-12-26 16:35:45 +0300

    added a home image

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -11,9 +11,19 @@ const Home = () => {
           <div className='home-left_title'>
             Let's Remain Connected.
           </div>
-          <div>Description</div>
+          <div className='home-left_description'>
+            EquiKonnect aim is to help equity alumni remain connected
+            by allowing them to share their experiences, achievements and progress.
+            It will also be a nice platform which equity leaders can use to share any 
+            upcoming events, hackathons and resources which can help alumni be upto to date.
+          </div>
+          <div className='home-btn'>
+            Get Started
+          </div>
         </div>
-        <div className='home-main_right'>Right</div>
+        <div className='home-main_right'>
+          <img src='./equikonnect.jpg' alt='equikonnect' />
+        </div>
       </div>
       <div>Footer</div>
     </div>
