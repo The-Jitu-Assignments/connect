9e00eba9b6a4fe0762d8abd16fb777af0b456f81
john katua <johnkatua99@gmail.com>
2022-12-26 20:14:13 +0300

    added a login and register buttons and showing the active button

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -3,7 +3,24 @@ import './Login.css';
 
 const Login = () => {
   return (
-    <div className='login-container'>Login</div>
+    <div className='login-container'>
+      <div className='login-form'>
+        <div className='login-title'>
+          <h2>EquiKonnect</h2>
+          <div>
+            <span className='login-title_btn active'>Login</span>
+            <span className='login-title_btn'>Register</span>
+          </div>
+        </div>
+        <h3>Sign In</h3>
+        <div>
+          Input 1
+        </div>
+        <div>
+          Input 2
+        </div>
+      </div>
+    </div>
   )
 }
 
