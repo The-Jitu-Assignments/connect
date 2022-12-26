eab53a8b2d2a4d860246cd3ad3601199376b55c6
john katua <johnkatua99@gmail.com>
2022-12-26 16:00:58 +0300

    added fixed height to home main component

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -6,7 +6,10 @@ const Home = () => {
   return (
     <div>
       <Header />
-      <div className='home-main'>Main</div>
+      <div className='home-main'>
+        <div className='home-main_left'>Left</div>
+        <div className='home-main_right'>Right</div>
+      </div>
       <div>Footer</div>
     </div>
   )
