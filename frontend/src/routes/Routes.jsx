e2c32ae34f4530ffa2bd03fea207956382874477
john katua <johnkatua99@gmail.com>
2022-12-26 18:58:59 +0300

    added a route component

---
+++ frontend/src/routes/Routes.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "../pages/home/Home"
+import Login from "../pages/login/Login"
+
+const RouteComponent = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default RouteComponent
