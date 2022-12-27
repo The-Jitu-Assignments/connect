58cb6ccc70c5bc7034f43e5dd05a66b940ab41c3
john katua <johnkatua99@gmail.com>
2022-12-27 23:50:52 +0300

    added placeholder to the inputs

--- frontend/src/pages/register/MultiPage/Second/index.jsx
+++ frontend/src/pages/register/MultiPage/Second/index.jsx
@@ -51,6 +51,7 @@ const Second = ({ userDetails, handlePrevious, setUserDetails }) => {
             type={"text"} 
             value={userDetails.name}
             name='name'
+            placeholder='Enter your name'
             onChange={(e) => {
               setUserDetails({
                 ...userDetails,
@@ -65,6 +66,7 @@ const Second = ({ userDetails, handlePrevious, setUserDetails }) => {
             type={"text"} 
             value={userDetails.password}
             name='password'
+            placeholder='Enter your password'
             onChange={(e) => {
               setUserDetails({
                 ...userDetails,
@@ -79,6 +81,7 @@ const Second = ({ userDetails, handlePrevious, setUserDetails }) => {
             type={"text"} 
             value={userDetails.phoneNumber}
             name='phoneNumber'
+            placeholder='Enter your phone number'
             onChange={(e) => {
               setUserDetails({
                 ...userDetails,
@@ -93,6 +96,7 @@ const Second = ({ userDetails, handlePrevious, setUserDetails }) => {
             type={"text"} 
             value={userDetails.county}
             name='county'
+            placeholder='Enter your county of residence e.g Lamu'
             onChange={(e) => {
               setUserDetails({
                 ...userDetails,
@@ -107,6 +111,7 @@ const Second = ({ userDetails, handlePrevious, setUserDetails }) => {
             type={"text"} 
             value={userDetails.branchName}
             name='branchName'
+            placeholder='Enter your equity branch e.g Voi Branch'
             onChange={(e) => {
               setUserDetails({
                 ...userDetails,
@@ -121,6 +126,7 @@ const Second = ({ userDetails, handlePrevious, setUserDetails }) => {
             type={"text"} 
             value={userDetails.school}
             name='school'
+            placeholder='Enter your school name e.g University of Eldoret'
             onChange={(e) => {
               setUserDetails({
                 ...userDetails,
@@ -130,7 +136,7 @@ const Second = ({ userDetails, handlePrevious, setUserDetails }) => {
           />
         </div>
         <div className='second-radio'>
-          <label>School level</label>
+          <label>School level:</label>
           <div>
             <input 
               type={"radio"} 
@@ -166,6 +172,7 @@ const Second = ({ userDetails, handlePrevious, setUserDetails }) => {
             type={"text"} 
             value={userDetails.course}
             name='course'
+            placeholder='Enter course details e.g Computer Science'
             onChange={(e) => {
               setUserDetails({
                 ...userDetails,
@@ -180,6 +187,7 @@ const Second = ({ userDetails, handlePrevious, setUserDetails }) => {
             type={"number"} 
             value={userDetails.period}
             name='period'
+            placeholder='Enter number of years spent in school'
             onChange={(e) => {
               setUserDetails({
                 ...userDetails,
@@ -189,7 +197,7 @@ const Second = ({ userDetails, handlePrevious, setUserDetails }) => {
           />
         </div>
         <div className='second-radio'>
-          <label>School level</label>
+          <label>Completion Status:</label>
           <div>
             <input 
               type={"radio"} 
