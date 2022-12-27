0091cfb2d33ed3ef591ac10843e7fd95432366cf
john katua <johnkatua99@gmail.com>
2022-12-27 18:28:11 +0300

    added next and previous buttons

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import First from './MultiPage/First';
+import Second from './MultiPage/Second';
 import './Register.css'
 
 const Register = () => {
   const navigate = useNavigate();
+  const [page, setPage] = useState(0);
+
+  const multiRegistrationComponent = () => {
+    switch (page) {
+      case 0:
+        return <First />;
+      case 1:
+        return <Second />
+      default:
+        return <First />
+    }
+  };
+
+  const handleSubmit = () => {
+    setPage(page + 1)
+  };
+
   return (
     <div className='register-container'>
       <div className='register-form'>
@@ -15,6 +34,11 @@ const Register = () => {
           </div>
         </div>
         <h3>Sign Up</h3>
+        {multiRegistrationComponent()}
+        {page > 0 && <button onClick={() => setPage(page - 1)}>Back</button>}
+        <button onClick={handleSubmit}>
+          {page === 0 ? 'Next' : 'Submit'}
+        </button>
       </div>
     </div>
   )
