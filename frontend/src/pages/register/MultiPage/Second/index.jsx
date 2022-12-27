bd616c763d8922cbe8a94b794371c09fb87c8e0a
john katua <johnkatua99@gmail.com>
2022-12-27 22:15:22 +0300

    added radio buttons to switch between alumni and admin

--- frontend/src/pages/register/MultiPage/Second/index.jsx
+++ frontend/src/pages/register/MultiPage/Second/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Second.css';
 import { BiArrowBack } from 'react-icons/bi'
 
-const Second = ({ userDetails, handlePrevious }) => {
+const Second = ({ userDetails, handlePrevious, setUserDetails }) => {
   return (
     <div>
       <div className='second-title'>
@@ -10,9 +10,38 @@ const Second = ({ userDetails, handlePrevious }) => {
           <BiArrowBack />
           <span>Back</span>
         </div>
-        <div className='second_userDetails'>
-          <div>{userDetails.email}</div>
-          <div>Admin</div>
+        <div className='second-userDetails'>
+          <div className='second-userDetails_email'>{userDetails.email}</div>
+          <div className='second-radio'>
+            <div>
+              <input 
+                type={"radio"} 
+                value={"alumni"} 
+                name='userStatus' 
+                checked={userDetails.userStatus === 'alumni'}
+                onChange={(e) => {
+                  setUserDetails({
+                    ...userDetails,
+                    [e.target.name]: e.target.value
+                  })
+                }}
+              /> Alumni
+            </div>
+            <div>
+              <input 
+                type={"radio"} 
+                value={"admin"} 
+                name='userStatus'
+                checked={userDetails.userStatus === 'admin'} 
+                onChange={(e) => {
+                  setUserDetails({
+                    ...userDetails,
+                    [e.target.name]: e.target.value
+                  })
+                }}
+              /> Admin
+            </div>
+          </div>
         </div>
       </div>
       <div>Form</div>
