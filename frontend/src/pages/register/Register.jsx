d45d241a603c94d96ffba590815c225ebdc1f4fe
john katua <johnkatua99@gmail.com>
2022-12-27 17:01:28 +0300

    user can switch between login and register page

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Register.css'
 
 const Register = () => {
+  const navigate = useNavigate();
   return (
-    <div className='register-container'>Register</div>
+    <div className='register-container'>
+      <div className='register-form'>
+        <div className='login-title'>
+          <h2 onClick={() => navigate('/')}>EquiKonnect</h2>
+          <div>
+            <span className='login-title_btn' onClick={() => navigate('/login')}>Login</span>
+            <span className='login-title_btn active'>Register</span>
+          </div>
+        </div>
+        <h3>Sign Up</h3>
+      </div>
+    </div>
   )
 }
 
