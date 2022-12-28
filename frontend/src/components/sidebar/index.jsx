bda7741b8ad92bf9a343f779780eba71ee3b82cb
john katua <johnkatua99@gmail.com>
2022-12-28 14:28:03 +0300

    added sidebar items

--- frontend/src/components/sidebar/index.jsx
+++ frontend/src/components/sidebar/index.jsx
@@ -3,7 +3,28 @@ import './sidebar.css'
 
 const Sidebar = ({ close }) => {
   return (
-    <div onClick={close} className='sidebar'>Sidebar</div>
+    <div className='sidebar'>
+      <div className='sidebar-header'>
+        <span onClick={close}>X</span>
+      </div>
+      <div className='sidebar-item_box'>
+        <div className="sidebar-item">
+          Home
+        </div>
+        <div className="sidebar-item">
+          Messages
+        </div>
+        <div className="sidebar-item">
+          Notifications
+        </div>
+        <div className="sidebar-item">
+          Profile
+        </div>
+        <div className="sidebar-item">
+          Data & Analytics
+        </div>
+      </div>
+    </div>
   )
 }
 
