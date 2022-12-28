b3ee0d3319a9ba37234f09092948af8564dc95dc
john katua <johnkatua99@gmail.com>
2022-12-28 11:40:27 +0300

    added a menu icon

--- frontend/src/layout/index.jsx
+++ frontend/src/layout/index.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
 import './layout.css'
+import { FcMenu } from 'react-icons/fc'
 
 const Layout = ({ children }) => {
   return (
     <div className='layout'>
-      <Header />
+      <div className='layout-header'>
+        <span>
+          <FcMenu fontSize={"2em"} />
+        </span>
+        <Header />
+      </div>
       <div className='main-content'>
         {children}
       </div>
