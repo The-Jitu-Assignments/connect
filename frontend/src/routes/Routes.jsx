404d568462a20c12e0bd85d82447476b03bf7c0a
john katua <johnkatua99@gmail.com>
2022-12-28 19:08:02 +0300

    added routes to different pages

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,8 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Layout from "../layout"
+import DataAndAnalyticsPage from "../pages/analytics"
 import HomePage from "../pages/home"
 import LandingPage from "../pages/landing/Landing"
 import Login from "../pages/login/Login"
+import MessagePage from "../pages/message"
+import NotificationsPage from "../pages/notification"
+import ProfilePage from "../pages/profile"
 import Register from "../pages/register/Register"
 
 const RouteComponent = () => {
@@ -15,6 +19,26 @@ const RouteComponent = () => {
         <Route path="/home" element={
           <Layout>
             <HomePage />
+          </Layout>
+        } />
+        <Route path="/message" element={
+          <Layout>
+            <MessagePage />
+          </Layout>
+        } />
+        <Route path="/notification" element={
+          <Layout>
+            <NotificationsPage />
+          </Layout>
+        } />
+        <Route path="/profile" element={
+          <Layout>
+            <ProfilePage />
+          </Layout>
+        } />
+        <Route path="/dataAndAnalytics" element={
+          <Layout>
+            <DataAndAnalyticsPage />
           </Layout>
         } />
       </Routes>
