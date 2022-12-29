b81328cb36ff640690ad1b273115b772eb2c4165
john katua <johnkatua99@gmail.com>
2022-12-29 06:58:07 +0300

    added an overflow y to the analytics page

--- frontend/src/pages/analytics/index.jsx
+++ frontend/src/pages/analytics/index.jsx
@@ -3,7 +3,24 @@ import './analytics.css'
 
 const DataAndAnalyticsPage = () => {
   return (
-    <div className='analytics-container'>DataAndAnalyticsPage</div>
+    <div className='analytics-container'>
+      <div>
+        <div>Search</div>
+        <div>Filter</div>
+      </div>
+      <div>
+        <div className="test">Item1</div>
+        <div className="test">Item2</div>
+        <div className="test">Item3</div>
+        <div className="test">Item4</div>
+        <div className="test">Item5</div>
+        <div className="test">Item6</div>
+        <div className="test">Item7</div>
+        <div className="test">Item8</div>
+        <div className="test">Item9</div>
+        <div className="test">Item10</div>
+      </div>
+    </div>
   )
 }
 
